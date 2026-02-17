f0788eb7a4f4cc21d338258d2e32adb61197411d
serrrfirat <f@nuff.tech>
2026-02-17 10:28:01 +0400

    chore: release synapse 0.1.5 formula
    Point Homebrew formula to v0.1.5 and update darwin arm64/x64 checksums for the Screenpipe startup fallback release.

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -1,7 +1,7 @@
 class Synapse < Formula
   desc "Ambient intelligence for your desktop"
   homepage "https://github.com/serrrfirat/homebrew-synapse"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   depends_on "screenpipe"
@@ -9,12 +9,12 @@ class Synapse < Formula
   on_macos do
     on_arm do
       url "https://github.com/serrrfirat/homebrew-synapse/releases/download/v#{version}/synapse-#{version}-darwin-arm64.tar.gz"
-      sha256 "6f07b19a123ba79834c1b85322c86cdae2d4c9079f04d09d42f95e06e9b1e3e9"
+      sha256 "f3c0d8b5b0ceed115bd778ac5aa94dd0818396944f255fb3879b00c734937662"
     end
 
     on_intel do
       url "https://github.com/serrrfirat/homebrew-synapse/releases/download/v#{version}/synapse-#{version}-darwin-x64.tar.gz"
-      sha256 "30115e0756e64a8701f0f5f04e0a766c1abae9794edddcf87bdae58acc5783d0"
+      sha256 "7c7ce5ef3991a4ecf5488a50819ede3d9e51754a0d20d81b0a44f1517684fcd7"
     end
   end
 
